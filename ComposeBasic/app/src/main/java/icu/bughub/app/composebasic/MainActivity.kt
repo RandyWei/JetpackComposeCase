dfcec25567b32a28243778a06f2a92b9391be71b
RandyWei <smile561607154@163.com>
2022-02-09 08:28:18 +0800

    add TextSample

--- ComposeBasic/app/src/main/java/icu/bughub/app/composebasic/MainActivity.kt
+++ ComposeBasic/app/src/main/java/icu/bughub/app/composebasic/MainActivity.kt
@@ -1,5 +1,6 @@
 package icu.bughub.app.composebasic
 
+import android.graphics.Color
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -9,12 +10,14 @@ import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
 import icu.bughub.app.composebasic.ui.theme.ComposeBasicTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContent {
             ComposeBasicTheme {
                 // A surface container using the 'background' color from the theme
@@ -34,7 +37,10 @@ fun Greeting(name: String) {
     Text(text = "Hello $name!")
 }
 
-@Preview(showBackground = true)
+@Preview(
+    showSystemUi = true,
+    name = "Preview Name"
+)
 @Composable
 fun DefaultPreview() {
     ComposeBasicTheme {
